1b713845b9c01eb09c712cc4e690a08b1da72592
Thalya Stéffany <87552041+thalya-codes@users.noreply.github.com>
2024-05-19 12:20:17 -0300

    feat: cria index

---
+++ index.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>ReadReach</title>
+    <link rel="stylesheet" href="src/css/global.css">
+</head>
+<body>
+    
+    <?php
+        echo "<h1>Título</h1>";
+    ?>
+</body>
+</html>
