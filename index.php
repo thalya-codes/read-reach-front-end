baf55444d0108d6f32dd56eca43ec0becd75d71c
Thalya Stéffany <87552041+thalya-codes@users.noreply.github.com>
2024-05-19 12:24:13 -0300

    feat: adiciona bootstrap

--- index.php
+++ index.php
@@ -4,12 +4,17 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>ReadReach</title>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
     <link rel="stylesheet" href="src/css/global.css">
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-YvpcrYf0tY3lHB60NNkmXc5s9fDVZLESaAA55NDzOxhy9GkcIdslK1eN7N6jIeHz" crossorigin="anonymous"></script>
 </head>
 <body>
     
     <?php
         echo "<h1>Título</h1>";
+    ?>
+    <?php 
+        echo "<button type='button' class='btn btn-primary'>Primary</button>";
     ?>
 </body>
 </html>
