e0df6de6141c54038307845163b20b97485d7533
Thalya Stéffany <87552041+thalya-codes@users.noreply.github.com>
2024-06-05 19:27:16 -0300

    feat(ReadindsPage): exporta handler functions

---
+++ src/pages/ReadingsPage/js/index.js
@@ -0,0 +1,5 @@
+import { handleDeleteReading } from "./handlers/handleDeleteReading.js";
+import "./handlers/handleCreateReading.js";
+import "./handlers/handleShowReadings.js";
+
+window.handleDeleteReading = handleDeleteReading;
