677f25d7c514584342be2ec6c7406d5d74da01a9
Thalya Stéffany <87552041+thalya-codes@users.noreply.github.com>
2024-06-05 19:07:29 -0300

    feat(services): cria método http get

--- src/js/services/http.js
+++ src/js/services/http.js
@@ -1,11 +1,16 @@
 import { BASE_URL } from "../constants.js";
 
 export const http = {
-  post: async (endpoint, data) => {
+  post: async (
+    endpoint,
+    data,
+    headers = { "Content-Type": "application/json" }
+  ) => {
     const response = await fetch(`${BASE_URL}${endpoint}`, {
       method: "POST",
       headers: {
         "Content-Type": "application/json",
+        ...headers,
       },
       body: JSON.stringify(data),
     });
@@ -13,5 +18,22 @@ export const http = {
     const responseData = await response.json();
 
     return responseData;
+  },
+
+  get: async (endpoint, options = {}) => {
+    const response = await fetch(`${BASE_URL}${endpoint}`, options);
+    const response_data = await response.json();
+
+    return response_data;
+  },
+
+  delete: async (endpoint, headers) => {
+    const response = await fetch(`${BASE_URL}${endpoint}`, {
+      method: "DELETE",
+      headers: {
+        "Content-Type": "application/json",
+        ...headers,
+      },
+    });
   },
 };
