39f8386a860bbf7829a5f0cd0e45f1689647136a
Thalya Stéffany <87552041+thalya-codes@users.noreply.github.com>
2024-06-05 19:28:27 -0300

    refactor(services): substitui string fixa(access_token) por constante

--- src/js/services/auth.js
+++ src/js/services/auth.js
@@ -1,9 +1,10 @@
+import { ACCESS_TOKEN_KEY } from "../constants.js";
 import { http } from "./http.js";
 
 export async function authenticateUser(data) {
   try {
     const response_data = await http.post("/login", data);
-    localStorage.setItem("access_token", response_data.token);
+    localStorage.setItem(ACCESS_TOKEN_KEY, response_data.token);
     return response_data;
   } catch (error) {
     console.log(error);
