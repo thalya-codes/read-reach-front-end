f86b8725d09b4c5e448325fe48cecc512ec59b73
Thalya Stéffany <87552041+thalya-codes@users.noreply.github.com>
2024-06-06 09:47:55 -0300

    feat(login): redireciona user para a página de leituras

--- src/pages/login/js/handleLogin.js
+++ src/pages/login/js/handleLogin.js
@@ -1,4 +1,4 @@
-import { authenticateUser } from "../../../js/services/auth.js";
+import { authenticateUser, checkAuth } from "../../../js/services/auth.js";
 
 const login_form = document.getElementById("login-form");
 
@@ -6,10 +6,19 @@ export async function handleLogin(event) {
   event.preventDefault();
   const [email, password] = event.target;
 
-  await authenticateUser({
+  const response = await authenticateUser({
     email: email.value,
     password: password.value,
   });
+
+  if (response.token) {
+    //TODO: Trocar URL
+    window.location.replace(
+      "http://localhost/read-reach-front-end/src/pages/ReadingsPage/index.php"
+    );
+  }
 }
 
 login_form.addEventListener("submit", handleLogin);
+
+document.addEventListener("DOMContentLoaded", checkAuth);
